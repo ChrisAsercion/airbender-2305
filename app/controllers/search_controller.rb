4fa0d911b5e824ae692946fb0269cf50859905ad
Chris Asercion <chris.asercion@gmail.com>
2023-09-08 10:13:55 -0600

    Completes the character details search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,9 @@
+class SearchController < ApplicationController
+  def index
+    conn = Faraday.new(url: 'https://last-airbender-api.fly.dev')
+    response = conn.get("/api/v1/characters?affiliation=#{params[:nation]}")
+    data = JSON.parse(response.body, symbolize_names: true)
+    @data25 = data.take(25)
+    @total = data.count
+  end
+end
---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Nation Search', type: :feature do
+  describe 'nation search feature' do
+    scenario 'I selected fire nation from the drop down list' do
+      visit root_path
+      
+      select "Fire Nation", from: 'nation'
+      click_on('Search For Members')
+      
+      expect(current_path).to eq(search_path)
+      
+
+      expect(page).to have_content("name: Azula")
+      expect(page).to have_content("affiliation = Azula's team (formerly)")
+      expect(page).to have_content("allies = Ozai Zuko")
+      expect(page).to have_content("enemies = Iroh Zuko")
+
+    end
+  end
+end
